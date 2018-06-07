60b654f6614f926f05579e0d49db6839d11e89f0
dominikglandorf <do.gl@gmx.de>
2018-06-07 17:33:05 +0200

    view mode

--- controllers/material.js
+++ controllers/material.js
@@ -14,7 +14,7 @@ const listWorksheets = function(req, res, next) {
       res.json(data);
     } else {
       const worksheets = data.map(worksheet => {
-        worksheet.url = 'edit/' + worksheet._id;
+        worksheet.url = 'view/' + worksheet._id;
         worksheet.secondaryTitle = `${worksheet.content.length} Blöcke`;
         worksheet.background = '#b10438';
         worksheet.content = '';
@@ -29,6 +29,18 @@ const listWorksheets = function(req, res, next) {
   });
 }
 
+const worksheetApi = `../api`;
+const ltiBaseUrl = '../../courses/X/tools/run/';
+
+const viewWorksheet = (req, res, next) => {
+  res.render('material/view-worksheet', {
+    title: 'Blatt anzeigen',
+    worksheet_api: worksheetApi,
+    worksheet_id: req.params.id,
+    lti_base_url: ltiBaseUrl
+  });
+};
+
 const createWorksheet = (req, res, next) => {
   api(req).post('/worksheet/', {
     json: {
@@ -41,34 +53,11 @@ const createWorksheet = (req, res, next) => {
 }
 
 const editWorksheet = (req, res, next) => {
-  // TODO: read users pseudonym
-  // let currentUser = res.locals.currentUser;
-  // Promise.all([
-  //   api(req).get('/ltiTools/' + req.params.ltiToolId),
-  //   api(req).get('/roles/' + currentUser.roles[0]._id),
-  //   api(req).get('/pseudonym?userId=' + currentUser._id + '&toolId=' + req.params.ltiToolId)
-  // ]).then(([tool, role, pseudonym]) => {
-  //   let customer = new ltiCustomer.LTICustomer();
-  //   let consumer = customer.createConsumer(tool.key, tool.secret);
-  //   let user_id = pseudonym.data[0].token; // use provider specific pseudonym instead of user_id
-  //   let payload = {
-  //     lti_version: tool.lti_version,
-  //     lti_message_type: tool.lti_message_type,
-  //     resource_link_id: tool.resource_link_id || req.params.courseId,
-  //     roles: customer.mapSchulcloudRoleToLTIRole(role.name),
-  //     launch_presentation_document_target: 'window',
-  //     launch_presentation_locale: 'en',
-  //     // lis_person_name_full: currentUser.displayName || `${currentUser.firstName} ${currentUser.lastName}`,
-  //     // lis_person_contact_email_primary: currentUser.username ? `${currentUser.username}@schul-cloud.org` : 'jbaird@uni.ac.uk',
-  //     user_id
-  //   };
-  // });
-
   res.render('material/edit-worksheet', {
       title: 'Blatt bearbeiten',
-      worksheet_api: `../api`,
+      worksheet_api: worksheetApi,
       worksheet_id: req.params.id,
-      lti_base_url: '../../courses/X/tools/run/'
+      lti_base_url: ltiBaseUrl
   });
 };
 
@@ -120,6 +109,7 @@ const getLtiRequest = (req, res, next) => {
 router.use(authHelper.authChecker);
 
 router.get('/', listWorksheets);
+router.get('/view/:id', viewWorksheet);
 router.get('/add/', createWorksheet);
 router.get('/edit/:id', editWorksheet);
 
